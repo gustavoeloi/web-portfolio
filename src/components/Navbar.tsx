0f7f8f62a0c94f43dfe08b8687b17dacfa0dcfb7
Gustavo Eloi <gustavooeloi@gmail.com>
2024-02-28 15:06:47 -0300

    feat: coding a responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,78 +1,56 @@
-import { motion, useAnimation } from "framer-motion";
 import { Menu } from "lucide-react";
 import { X } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+const navLinks = [
+  { id: 1, label: "Home", to: "/" },
+  { id: 2, label: "Projects", to: "/projects" },
+  { id: 3, label: "About", to: "/about" },
+  { id: 4, label: "Contact", to: "/contact" },
+];
+
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const controls = useAnimation();
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-    controls.start({ opacity: isMenuOpen ? 0 : 1, x: isMenuOpen ? -100 : 0 });
-  };
 
   return (
-    <header className="flex flex-wrap sm:justify-center sm:flex-nowrap z-50 w-full text-2xl md:text-3xl text-gray-400 py-8 sm:py-16">
-      <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
-        <div className="flex items-center justify-between">
-          <a
-            className="flex-none text-3xl md:text-5xl text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 font-extrabold"
-            href="/"
-          >
+    <header className="w-full  text-white bg-black fixed">
+      <div className="flex justify-between items-center h-32 px-4 py-12">
+        <div>
+          <h1 className="ml-2 font-extrabold text-transparent text-2xl md:text-5xl bg-clip-text bg-gradient-to-r from-cyan-600 to-teal-600 p-2 font-signature">
             Gustavo Eloi
-          </a>
-          <div className="sm:hidden">
-            <button
-              className="text-gray-600 focus:outline-none"
-              onClick={toggleMenu}
-            >
-              {!isMenuOpen ? <Menu size={24} /> : <X size={24} />}
-            </button>
-          </div>
+          </h1>
         </div>
-        <motion.div
-          animate={controls}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 0.3, ease: "easeInOut" }}
-          className="sm:hidden mt-4 sm:mt-0"
+
+        <ul className="hidden md:flex">
+          {navLinks.map((item) => (
+            <Link key={item.id} to={item.to}>
+              <li className="px-4 cursor-pointer text-2xl capitalize font-medium text-gray-500 hover:scale-105 duration-200">
+                {item.label}
+              </li>
+            </Link>
+          ))}
+        </ul>
+
+        <div
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
         >
-          <ul className="flex flex-col gap-16 mt-8 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
-            <motion.li
-              className="font-medium hover:text-gray-200"
-              onClick={toggleMenu}
-            >
-              <Link to="/projects">Projetos</Link>
-            </motion.li>
-            <motion.li
-              className="font-medium hover:text-gray-200"
-              onClick={toggleMenu}
-            >
-              <Link to="/about">Sobre</Link>
-            </motion.li>
-            <motion.li
-              className="font-medium hover:text-gray-200"
-              onClick={toggleMenu}
-            >
-              <Link to="/contact">Contato</Link>
-            </motion.li>
-          </ul>
-        </motion.div>
-        <div className="hidden basis-full grow sm:block">
-          <ul className="flex flex-col gap-16 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
-            <li className="font-medium hover:text-gray-200">
-              <Link to="/projects">Projetos</Link>
-            </li>
-            <li className="font-medium hover:text-gray-200">
-              <Link to="/about">Sobre</Link>
-            </li>
-            <li className="font-medium hover:text-gray-200">
-              <Link to="/contact">Contato</Link>
-            </li>
-          </ul>
+          {isMenuOpen ? <X size={32} /> : <Menu size={32} />}
         </div>
-      </nav>
+
+        {isMenuOpen && (
+          <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+            {navLinks.map((item) => (
+              <Link key={item.id} to={item.to}>
+                <li className="px-4 cursor-pointer capitalize py-6 text-4xl">
+                  {item.label}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        )}
+      </div>
     </header>
   );
 };
