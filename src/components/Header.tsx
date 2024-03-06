06702988541d71eff2ff5ca146b14f48a707bf88
gustavooeloi <gustavooeloi@gmail.com>
2024-03-06 19:12:56 -0300

    feat: animation on navbar with react-spring

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 import { Link } from "react-scroll";
-import MenuMobile from "./MenuMobile";
 
 import emojiComputer from "@/assets/images/EmojiComputer.png";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import { useSpring, animated } from "react-spring";
 
 const navLinks = [
   { id: 1, label: "About", to: "about" },
@@ -10,8 +12,15 @@ const navLinks = [
 ];
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuAnimation = useSpring({
+    transform: isMenuOpen ? "translateX(0%)" : "translateX(100%)",
+    opacity: isMenuOpen ? 1 : 0,
+  });
+
   return (
-    <header className=" fixed flex items-center justify-between w-full text-base py-4 lg:px-10 backdrop-blur-sm border-b border-teal-300 bg-gradient-to-r bg-black from-transparent via-blue-300/20 to-transparent z-50">
+    <header className="fixed flex items-center justify-between w-full text-base py-4 lg:px-10 backdrop-blur-sm border-b border-teal-300 bg-gradient-to-r bg-black from-transparent via-blue-300/20 to-transparent z-50">
       <div className="flex px-3 items-center">
         <div className="flex items-center justify-center gap-2">
           <img src={emojiComputer} className="w-14 h-14" alt="Emoji Computer" />
@@ -20,28 +29,42 @@ const Header = () => {
           </h1>
         </div>
       </div>
+      <ul className="hidden md:flex">
+        {navLinks.map((item) => (
+          <Link key={item.id} to={item.to} smooth={true} duration={500}>
+            <li className="px-4 cursor-pointer text-2xl capitalize font-medim text-gray-400 hover:scale-105 duration-200">
+              {item.label}
+            </li>
+          </Link>
+        ))}
+      </ul>
 
-      <div className="lg:hidden">
-        <MenuMobile />
+      <div
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {isMenuOpen ? <X size={32} /> : <Menu size={32} />}
       </div>
 
-      <nav className="hidden lg:block">
-        <ul className="lg:flex justify-around gap-3">
-          {navLinks.map((item) => (
-            <Link
-              key={item.id}
-              to={item.to}
-              smooth={true}
-              duration={500}
-              offset={-70}
-            >
-              <li className="px-4 cursor-pointer text-2xl capitalize font-medim text-gray-400 hover:scale-105 duration-200">
-                {item.label}
-              </li>
-            </Link>
-          ))}
-        </ul>
-      </nav>
+      <animated.ul
+        style={menuAnimation}
+        className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 z-100"
+      >
+        {navLinks.map((item) => (
+          <Link
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            key={item.id}
+            to={item.to}
+            smooth={true}
+            duration={500}
+            offset={-100}
+          >
+            <li className="px-4 cursor-pointer capitalize py-6 text-4xl">
+              {item.label}
+            </li>
+          </Link>
+        ))}
+      </animated.ul>
     </header>
   );
 };
--- src/components/MenuMobile.tsx
+++ src/components/MenuMobile.tsx
@@ -8,6 +8,7 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Link } from "react-scroll";
+import { useState } from "react";
 
 const navLinks = [
   { id: 3, label: "About", to: "about" },
@@ -16,9 +17,18 @@ const navLinks = [
 ];
 
 const MenuMobile = () => {
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+
+  const closeDrawer = () => {
+    setDrawerOpen(false);
+  };
+
   return (
-    <Drawer direction="right">
-      <DrawerTrigger className="px-5">
+    <Drawer direction="right" open={isDrawerOpen}>
+      <DrawerTrigger
+        className="px-5"
+        onClick={() => setDrawerOpen(!isDrawerOpen)}
+      >
         <AlignJustify size={30} className="text-white" />
       </DrawerTrigger>
       <DrawerContent className="h-full bg-gradient-to-t from-main/20 to-transparent rounded-none">
@@ -29,7 +39,13 @@ const MenuMobile = () => {
         <nav>
           <ul className="flex flex-col justify-around gap-3">
             {navLinks.map((item) => (
-              <Link key={item.id} to={item.to} smooth={true} duration={500}>
+              <Link
+                key={item.id}
+                to={item.to}
+                smooth={true}
+                duration={500}
+                onClick={closeDrawer}
+              >
                 <li className="px-4 cursor-pointer text-2xl capitalize font-medim text-gray-400 hover:scale-105 duration-200">
                   {item.label}
                 </li>
